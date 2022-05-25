56e2791e980444f41e1166f6d84894f0c4877a52
WillisCorp <61987188+WillisCorp@users.noreply.github.com>
2022-05-25 06:34:28 -0500

    Update ObtenerContenido.php

--- php/ObtenerContenido.php
+++ php/ObtenerContenido.php
@@ -3,21 +3,12 @@
 include("../php/ConexionBD.php");
 $ID = $_REQUEST["data"];
 $Query = "SELECT * FROM contenidorss where idRSS= '$ID' order by fecha DESC";
-
+$execute = mysqli_query($conexionBD, $Query);
 $data = array();
 
-$maxage = 60; 
-if (filemtime("online1.txt") < (time() - $maxage)) {
-$execute = mysqli_query($conexionBD, $Query);
 while ($row = mysqli_fetch_assoc($execute)) {
     $data[] = $row;
 }
-file_put_contents("online1.txt", $data);
-$datareserv[]=$data;
-}else {
-	
-$datareserv[]= file_get_contents("online1.txt");
-}
-echo json_encode($datareserv, JSON_UNESCAPED_SLASHES);
+echo json_encode($data, JSON_UNESCAPED_SLASHES);
 
 ?>
